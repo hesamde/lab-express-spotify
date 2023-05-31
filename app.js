7410d62053bc6dbded26b62ec6f28b3421f3ad91
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-05-31 07:49:03 +0200

    lab

--- app.js
+++ app.js
@@ -10,18 +10,15 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
 
-// Retrieve an access tokenspotifyApi
 spotifyApi
   .clientCredentialsGrant()
   .then((data) => {
     spotifyApi.setAccessToken(data.body["access_token"]);
-    // Make your API requests here
   })
   .catch((error) =>
     console.log("Something went wrong when retrieving an access token", error)
